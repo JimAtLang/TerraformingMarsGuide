783688e70e13af681f1527fa56f407606fe3f9fe
Jim Stewart <jim.stewart@thelangschool.org>
2025-02-04 10:30:55 -0500

    added border and mouseover

---
+++ script.js
@@ -0,0 +1,3 @@
+board_image = document.getElementById("board_image");
+board_image.addEventListener("mouseover", function(){this.style.borderColor = "red"});
+board_image.addEventListener("mouseout", function(){this.style.borderColor = "black"});
